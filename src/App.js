c47b7bdd474f9c17149733b29d0d60f850e2a75b
Dhrubot <dhrubo.dh@gmail.com>
2021-01-13 11:50:44 -0500

    rendered the movie title and year on the screen

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ const App = () => {
   const API_KEY = 'a48618b7'
   const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=time&page=1`
 
-  const [movies, setMovies] = useState([])
+  const [movieList, setMovieList] = useState([])
 
   useEffect(() => {
-    fetch(API_URL).then(res => res.json()).then(data => console.log(data.Search))
+    loadData()
   }, [])
+
+  const loadData = async () => {
+    const response = await fetch(API_URL)
+    const result = await response.json()
+    setMovieList(result.Search)
+  }
 
   return (
     <div>
       <h1>Hello, This is a fetch demo using React's useEffect hook.</h1>
+      <ul>
+        { movieList.map((movie, idx) => <li key={idx}>{movie.Title} ------ {movie.Year}</li>)}
+      </ul>
     </div>
   )
 }
