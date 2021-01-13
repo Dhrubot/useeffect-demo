9a420036f90f001b84c759c200d7e59f03eb18c2
Dhrubot <dhrubo.dh@gmail.com>
2021-01-13 10:58:35 -0500

    Added the api url and api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const App = () => {
+
+  const API_KEY = a48618b7
+  const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=time&page=1`
+
   return (
     <div>
       <h1>Hello, This is a fetch demo using React's useEffect hook.</h1>
