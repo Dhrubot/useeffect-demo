c99c9ea8917d75c534f541afd7169acb399742b8
Dhrubot <dhrubo.dh@gmail.com>
2021-01-13 11:16:00 -0500

    added states and useeffect hook with fetch and looking up data with console log

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
 
-  const API_KEY = a48618b7
+  const API_KEY = 'a48618b7'
   const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=time&page=1`
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL).then(res => res.json()).then(data => console.log(data.Search))
+  }, [])
 
   return (
     <div>
